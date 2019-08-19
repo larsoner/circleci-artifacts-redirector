f9c4aa9b6ace30e44b27e9f63022903998376acb
Eric Larson <larson.eric.d@gmail.com>
2019-08-19 15:36:55 -0400

    ENH: Specify path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// Checks API example
-// See: https://developer.github.com/v3/checks/ to learn more
+// This as annoying because CircleCI does not use the App API.
+// Hence we must monitor statuses rather than using the more convenient
+// "checks" API.
 
 /**
  * This is the main entrypoint to your Probot app
@@ -8,28 +9,42 @@
 module.exports = app => {
   app.on(['status'], link)
 
-  /* This is annoying because CircleCI does not use the App API we need to monitor statuses */
   async function link (context) {
     if (context.payload.context !== 'ci/circleci: build' || context.payload.state === 'pending') {
+      context.log('Ignoring:')
+      context.log(context.payload.context)
+      context.log(context.payload.state)
       return
     }
+    context.log('Processing:')
+    context.log(context.payload.context)
+    context.log(context.payload.state)
+    // Adapted (MIT license) from https://github.com/Financial-Times/ebi/blob/master/lib/get-contents.js
+    const filepath = '.circleci/artifact_path'
+    var path = ''
+    try {
+      const repoData = await context.github.repos.getContents(context.repo({ ref: context.payload.sha, path: filepath }))
+      path = Buffer.from(repoData.data.content, 'base64').toString('utf8')
+    } catch (error) {
+      if (error.status === 404) {
+        throw new Error(`404 ERROR: file '${filepath}' not found`)
+      } else {
+        throw error
+      }
+    }
+    // Set the new status
     const state = (context.payload.state === 'success') ? 'success' : 'neutral'
     const buildId = context.payload.target_url.split('?')[0].split('/').slice(-1)[0]
     const repoId = context.payload.repository.id
-    const url = 'https://' + buildId + '-' + repoId + '-gh.circle-artifacts.com/0/test_artifacts/README.md'
-    // Create the status
+    const url = 'https://' + buildId + '-' + repoId + '-gh.circle-artifacts.com/' + path
+    context.log('Linking to:')
+    context.log(url)
     return context.github.repos.createStatus(context.repo({
       sha: context.payload.sha,
       state: state,
       target_url: url,
-      description: 'CircleCI artifacts link',
-      context: 'ci/circleci: artifacts'
+      description: 'Link to ' + path,
+      context: 'ci/circleci: artifact'
     }))
   }
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
